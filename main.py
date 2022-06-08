1cca10152fce4bc5c70388743af2bed754c42fb6
acciolym <accioly@gmail.com>
2022-06-08 16:05:46 -0300

    Logo maior

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from util import bd
 import os
 from flask import Flask, render_template, flash, request, redirect, url_for
@@ -10,17 +9,4 @@ app = Flask(__name__)
 def menu():
   return render_template('home.html')
 
-=======
-from util import bd
-import os
-from flask import Flask, render_template, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
-
-app = Flask(__name__)
-
-@app.route('/')
-def menu():
-  return render_template('home.html')
-
->>>>>>> 37d2cc99831ee1c718b63e252890e21e7f4df067
 app.run(debug=True)
