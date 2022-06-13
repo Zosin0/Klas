b0001a5d98b63d0d26d8839b9f9db3d8cc7c2fc5
Lucas Zoser <53053622+Zosin0@users.noreply.github.com>
2022-06-13 18:47:55 -0300

    /'s atualizadas

--- DATABASE/script.sql
+++ DATABASE/script.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS Klas;
 CREATE DATABASE Klas;
 USE Klas;
 
@@ -50,15 +51,15 @@ CREATE TABLE tbCursoPago(
 -- INSERT ALUNOS (COMPLETO) --
 INSERT INTO GrupoKlas(nmeAluno, raAluno, descAluno, dsc_path_imagem_aluno) VALUES
 ("Lucas Zoser Nunes Costa", "22105593", "Project Management: Apto a organizar o grupo dentro dos prazos, e ajudar em todas as etapas de códigos para que o projeto progrida e consiga ser entregue a tempo."
-, "\\static\\imagens\\lucasz.png"),
+, "/static/imagens/lucasz.png"),
 ("Leonardo Benttes Almeida Placido dos Santos", "22103833", "Back-End Developer: Apto a entregar a main.py com todas as funcionalidades especificadas e ajudar todos os integrantes com o funcionamento do site"
-, "\\static\\imagens\\leob.png"),
+, "/static/imagens/leob.png"),
 ("Leonardo Areias Rodovalho", "221570", "Test Analyst DBA: Apto a separar os conteúdos do site, e popular as tabelas do banco de dados, fazendo assim as integrações entre linguagens da aplicação."
-, "\\static\\imagens\\leos.png"),
+, "/static/imagens/leos.png"),
 ("Pedro Henrique Moreira da Silva", "22201914", "Front-End Analyst: Apto a auxiliar todo o grupo em todas partes do site, garantindo que ele tenha todas as funções/conteúdos que foram especificados."
-,"\\static\\imagens\\pedrom.png"),
+,"/static/imagens/pedrom.png"),
 ("Matheus Soares Accioly", "22908908", "Front-End Developer: Apto a dar vida ao esqueleto do projeto, tornar o site utilizável e agradável para o usuário final, entregar os códigos de HTML e CSS dentro do que foi especificado."
-,"\\static\\imagens\\matheusa.png");
+,"/static/imagens/matheusa.png");
 
 
 -- INSERT PLATAFORMAS--
@@ -79,19 +80,18 @@ INSERT INTO tbPlataformas(nmePlataformas, tipoCursos, descPlataformas, link_plat
 ("Free Code Camp", "GRÁTIS", "Plataforma Web de aprendizagem interativa, um fórum de comunidade on-line, salas de bate-papo, publicações Medium e organizações locais que pretendem tornar a aprendizagem de desenvolvimento web acessível à qualquer pessoa.",
  "https://www.freecodecamp.org/", "/static/imagens/freec.png"),
 ("DankiCode", "PAGOS", "Plataforma que oferece vários pacotes com cursos de qualidade, cobrindo várias áreas da programação, e incluí a possibilidade de você mesmo ensinar!", 
-"https://cursos.dankicode.com/", "/static/imagens/dankic"),
+"https://cursos.dankicode.com/", "/static/imagens/dankic.png"),
 ("Alura", "PAGOS", "Possui um custo-benefício e qualidade inigualável, uma das melhores plataformas para se aprender programação.", 
-"https://www.alura.com.br/", "/static/imagens/alura"),
+"https://www.alura.com.br/", "/static/imagens/alura.png"),
 ("Udemy", "PAGOS", "Não só oferece cursos de programação, mas de todas as ferramentas possíveis, contando com ótima qualidade e preço acessível.", 
-"https://www.udemy.com/", "/static/imagens/udemy"),
+"https://www.udemy.com/", "/static/imagens/udemy.png"),
 ("RocketSeat", "PAGOS/GRATIS", "Conta com 3 programas que ensinam muitos conceitos e linguagens de programação, focando principalmente no desenvolvimento web, inclusive o (discover) que é gratuito.", 
-"https://www.rocketseat.com.br/", "/static/imagens/rocketseat"),
+"https://www.rocketseat.com.br/", "/static/imagens/rocketseat.png"),
 ("Pluralsight", "PAGOS", "É uma opção mais cara, contudo conta com cursos em inglês e profissionais de alto nível.", 
-"https://www.pluralsight.com/", "/static/imagens/pluralsight"),
+"https://www.pluralsight.com/", "/static/imagens/pluralsight.png"),
 ("Udacity", "PAGOS", "Oferece cursos dos mais complexos assuntos de programação, aulas em inglês e conteúdo de qualidade.", 
-"https://www.udacity.com/", "/static/imagens/udacity");
+"https://www.udacity.com/", "/static/imagens/udacity.png");
 
---INSERT LINGUAGENS --
-
+-- INSERT LINGUAGENS --
 
 SELECT * FROM GrupoKlas;
